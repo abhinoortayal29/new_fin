88920893c793ed351fdf504e5d390c91842bb39f
Abhinoor Tayal <abhinoortayal29@gmail.com>
2025-10-12 12:21:21 +0530

    final: ready for Vercel deployment

--- next.config.mjs
+++ next.config.mjs
@@ -1,20 +1,25 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-      images: {
-        remotePatterns: [
-          {
-            protocol: "https",
-            hostname: "randomuser.me",
-          },
-        ],
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "randomuser.me",
       },
-    
-      experimental: {
-        serverActions: {
-          bodySizeLimit: "5mb",
-        },
-      },
-    };
-    
-    export default nextConfig;
-    
+    ],
+  },
+
+  experimental: {
+    serverActions: {
+      bodySizeLimit: "5mb",
+    },
+  },
+
+  eslint: {
+    // false = fail builds on lint errors
+    // true = ignore lint errors temporarily
+    ignoreDuringBuilds: false
+  },
+};
+
+export default nextConfig;
